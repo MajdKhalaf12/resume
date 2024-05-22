49e295e25101a20237efcaeeef53441b043f2243
MajdAlkhalaf12 <majd03.hussain@gmail.com>
2024-05-22 20:40:56 +0300

    config edits

--- data/index.ts
+++ data/index.ts
@@ -108,7 +108,13 @@ export const projects = [
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
     img: "./p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    iconLists: [
+      "./re.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./three.svg",
+      "./fm.svg",
+    ],
     link: "https://github.com/adrianhajdin?tab=repositories",
   },
   {
@@ -116,7 +122,13 @@ export const projects = [
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "./p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./stream.svg",
+      "./c.svg",
+    ],
     link: "https://github.com/adrianhajdin/zoom-clone",
   },
   {
@@ -124,7 +136,7 @@ export const projects = [
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "./p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
     link: "https://github.com/adrianhajdin/ai_saas_app",
   },
   {
@@ -132,7 +144,13 @@ export const projects = [
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "./p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./three.svg",
+      "./gsap.svg",
+    ],
     link: "https://github.com/adrianhajdin/iphone",
   },
 ];
@@ -237,17 +255,17 @@ export const workExperience = [
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
     href: "https://github.com/MajdKhalaf12",
   },
   {
     id: 2,
-    img: "/facebook.svg",
+    img: "./facebook.svg",
     href: "https://www.facebook.com/majd.alkhalaf12/",
   },
   {
     id: 3,
-    img: "/link.svg",
+    img: "./link.svg",
     href: "https://www.linkedin.com/in/majd-al-khalaf",
   },
 ];
